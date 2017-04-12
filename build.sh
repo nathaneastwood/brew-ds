45e6f8e53da3cb105374b0b794635d2078251b79
garethclews <gareth.clews@ons.gov.uk>
2017-04-12 13:13:27 +0100

    python added

--- build.sh
+++ build.sh
@@ -1,6 +1,35 @@
-# Install brew
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-# Install from Brewfile
-brew bundle
-# Install common R packages
-Rscript -e "install.packages(c('tidyverse', 'rmarkdown', 'shiny', 'mlr'), repos = 'https://cloud.r-project.org/')"
+#!/usr/bin/env bash
+
+# Install brew, use brew to install system files and then use R and python
+# package managers to install their recommended packages/modules
+
+## check internet connection
+wget -q --tries=3 --timeout=5 --spider http://google.com
+if [[ $? -eq 0 ]]; then
+  echo "* Installing software"
+  alias install_brew=/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+  ## Make sure your PATH variable is as we expect, and it knows where brew lives
+  echo "export PATH=/usr/local/bin:/usr/local/sbin:$PATH" > ~/.profile
+  source ~/.profile
+
+  ## Refresh your command index
+  hash -r
+
+  ## check to see if it already exists, if it does skip installation
+  command -v brew >/dev/null && echo "* brew exists, skipping" || install_brew
+
+  # Install from Brewfile
+  brew bundle 1>install.log
+
+  # Install common R packages
+  Rscript "install.packages(c('tidyverse', 'rmarkdown', 'shiny'))" 1>install.log
+
+  # Install common python modules
+  pip3 install -r requirements.txt 1>install.log
+
+  echo "** Install complete"
+
+else
+  echo "* There is no internet connection, exiting."
+fi
