eed23ee593cb1e4e1c30ab79914053e6b1c759d3
nathaneastwood <nathan.eastwood@icloud.com>
2017-04-12 08:51:23 +0100

    Add brew installation

--- build.sh
+++ build.sh
@@ -1,2 +1,6 @@
+# Install brew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+# Install from Brewfile
 brew bundle
+# Install common R packages
 Rscript -e "install.packages(c('tidyverse', 'rmarkdown', 'shiny'))"
