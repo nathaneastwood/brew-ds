9fbb6be8218f15c133fdf8fbd8940c6d38d5215b
garethclews <notesoncircumscribingthesky@gmail.com>
2017-05-04 13:19:52 +0100

    rscript -> R

--- build.sh
+++ build.sh
@@ -90,7 +90,7 @@ if [[ $? -eq 0 ]]; then
 
   # Install common R packages (I work too hard, redirecting outputs...)
   echo "    -- installing R packages\n       \u25B3 this can take some time"
-  Rscript -e <<'  END'
+  R --no-save <<'  END'
     for (package %in% c('tidyverse', 'rmarkdown', 'shiny', 'mlr')) {
       message(paste("    \u25b3 Installing ", package))
       sink("install.log", append=TRUE)
